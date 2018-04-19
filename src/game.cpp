1fa306a483325f0b8bf54a0834d3bb86bacb7728
Max William Souto Filgueira <maxsnv@A307-32L.imd.local>
2018-04-19 07:33:11 -0300

    vamo la

--- src/game.cpp
+++ src/game.cpp
@@ -1,23 +1,24 @@
 #include "Gen.hpp"
 #include <iostream>
 #include <vector>
-void Update(std::vector <Gen> Game, int Gen_number )
+void Update(std::vector <Gen> &Game)
 {
   int count;
-  Gen dump(Game[Gen_number].Linhas(),Game[Gen_number].Colunas());
+  int index = Game.size()-1;
+  Gen dump(Game[index].Linhas(),Game[index].Colunas());
   Game.push_back(dump);
-  for (int i = 0; i < Game[Gen_number].Linhas(); ++i) {
-    for (int j = 0; j < Game[Gen_number].Colunas(); ++j) {
+  for (int i = 0; i < Game[index].Linhas(); ++i) {
+    for (int j = 0; j < Game[index].Colunas(); ++j) {
 
-      count = Game[Gen_number].NeighborsCount(i,j);
+      count = Game[index].NeighborsCount(i,j);
 
-      if(Game[Gen_number](i,j)){
+      if(Game[index](i,j)){
         if(count < 2 or count > 3){
-          Game[Gen_number +1 ].Death(i,j);
+          Game[index +1 ].Death(i,j);
         }
       }
       else if(count == 3) {
-        Game[Gen_number +1 ].Birth(i,j);
+        Game[index +1 ].Birth(i,j);
 
       }
     }
@@ -29,29 +30,28 @@ void Update(std::vector <Gen> Game, int Gen_number )
 
 int main(int argc, char *argv[])
 {
-   Gen dump(8,8);
-  // dump.Print();
+  Gen dump(8,8);
+  // Try[0].Print();
   std::vector<Gen> Try;
-  //  Try.push_back(dump);
-  dump;
-  dump.Birth(2, 2);
-  dump.Birth(2, 4);
-  dump.Birth(3, 2);
-  dump.Birth(3, 3);
-  dump.Birth(3, 4);
-  dump.Birth(4, 2);
-  dump.Birth(4, 4);
-  Gen dump2 = dump;
+  Try.push_back(dump);
+  Try[0].Birth(2, 2);
+  Try[0].Birth(2, 4);
+  Try[0].Birth(3, 2);
+  Try[0].Birth(3, 3);
+  Try[0].Birth(3, 4);
+  Try[0].Birth(4, 2);
+  Try[0].Birth(4, 4);
+  Update(Try);
   for (int i = 0; i < 8; ++i) {
     for (int j = 0; j < 8; ++j) {
-      if(dump2(i,j) > 0){
-        std::cout << "i " << i << " j " << j << " Neighbors: " << dump2.NeighborsCount(i,j) << std::endl;
+      if(Try[0](i,j) > 0){
+        std::cout << "i " << i << " j " << j << " Neighbors: " << Try[0].NeighborsCount(i,j) << std::endl;
       }
     }
 
   }
 
-  dump2.Print();
-  // Try[1].Print();
+  Try[0].Print();
+  Try[1].Print();
   return 0;
 }
