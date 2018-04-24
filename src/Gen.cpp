6840bcf9dc97cc1d283f084546cb3ae08722876b
ebetcel <emanuelbetcel@gmail.com>
2018-04-24 00:45:57 -0300

    fixing emacs problem

--- src/Gen.cpp
+++ src/Gen.cpp
@@ -4,129 +4,125 @@
  * \param int nCol : The number of columns in the Grid 
   */
 Gen::Gen (int nLin, int nCol)
+{
+	assert(nLin >= 0);
+	assert(nCol >= 0);
+	NLin = nLin;
+	NCol = nCol;
 
-  {
+	Grid = new bool *[nLin];
 
-    assert(nLin >= 0);
-    assert(nCol >= 0);
-    NLin = nLin;
-    NCol = nCol;
+	for (int i = 0; i < nLin; ++i) {
+		Grid[i] = new bool[nCol];
+	}
 
-    Grid = new bool *[nLin];
+	for (int i = 0; i < NLin; ++i) {
+		for (int j = 0; j < NCol; ++j) {
+			Grid[i][j]  = false;
+		}
+	}
+}
 
-    for (int i = 0; i < nLin; ++i) {
-      Grid[i] = new bool[nCol];
-    }
-
-    for (int i = 0; i < NLin; ++i) {
-      for (int j = 0; j < NCol; ++j) {
-        Grid[i][j]  = false;
-      }
-    }
-  }
 /*! Copy Constructor
  * \param const Gen &gen : The object to be copied
  * */
 Gen::Gen(const Gen &gen)
 {
- NLin = gen.NLin;
- NCol = gen.NCol;
+	NLin = gen.NLin;
+	NCol = gen.NCol;
 
- Grid = new bool *[gen.NLin];
+	Grid = new bool *[gen.NLin];
 
- for (int i = 0; i < gen.NLin; ++i) {
-   Grid[i] = new bool[gen.NCol];
- }
+	for (int i = 0; i < gen.NLin; ++i) {
+		Grid[i] = new bool[gen.NCol];
+	}
 
- for (int i = 0; i < gen.NLin; ++i) {
-	 for (int j = 0; j < gen.NCol; ++j) {
-     Grid[i][j] = gen.Grid[i][j];
-	 }
- }
+	for (int i = 0; i < gen.NLin; ++i) {
+		for (int j = 0; j < gen.NCol; ++j) {
+			Grid[i][j] = gen.Grid[i][j];
+	 	}
+	}
 }
 /*! The Class Destructor */
 Gen::~Gen()  
-  {
+{
 	for (int i = 0; i < NLin; ++i) {
 		delete [] Grid[i];
 	}
 	delete [] Grid;
-  }
+}
 
 int Gen::NeighborsCount(int Line,int Col)
-  {
-    int count = 0;
+{
+	int count = 0;
 
-    for (int i = -1; i <=1; ++i) {
-      for (int j = -1; j <=1; ++j) {
-        if(( Line+i < NLin and Col+j < NCol  ) and (Line+i >= 0 and Col+j >= 0)){
-          if(!(i == 0 and j == 0)){
-          count += Grid[Line+i][Col+j];
-          }
-        }
-      }
+	for (int i = -1; i <=1; ++i) {
+		for (int j = -1; j <=1; ++j) {
+			if(( Line+i < NLin and Col+j < NCol  ) and (Line+i >= 0 and Col+j >= 0)){
+				if(!(i == 0 and j == 0)){
+					count += Grid[Line+i][Col+j];
+				}
+			}
+		}
 
-    }
-    return count;
-  }
+	}
+	return count;
+}
 
 void Gen::Birth(int Line,int Col)
-  {
-    Grid[Line][Col] = true;
-  }
+{
+	Grid[Line][Col] = true;
+}
+
 void Gen::Death(int Line, int Col)
 {
-  Grid[Line][Col] = false;
+	Grid[Line][Col] = false;
 }
 
 bool Gen::operator()(int Line, int Col)
 {
-  assert(Line >= 0 && Line < NLin);
-  assert(Col >= 0 && Col < NCol);
-  return Grid[Line][Col];
+	assert(Line >= 0 && Line < NLin);
+	assert(Col >= 0 && Col < NCol);
+	return Grid[Line][Col];
 }
 
 Gen& Gen::operator=(const Gen &rhs)
 {
-  for (int i = 0; i < NLin; ++i) {
-    for (int j = 0; j < NCol; ++j) {
-      Grid[i][j] = rhs.Grid[i][j];
-    }
-
-  }
-  return *this;
+	for (int i = 0; i < NLin; ++i) {
+		for (int j = 0; j < NCol; ++j) {
+			Grid[i][j] = rhs.Grid[i][j];
+		}
+	}
+	return *this;
 }
 
 bool Gen::operator ==(const Gen & rhs)
 {
-  for(int i = 0; i < NLin; i++){
-    for(int j = 0; j < NCol; j++){
-      if (Grid[i][j] != rhs.Grid[i][j]){
-        return false;
-      }
-    }
-  }
-  return true;
+	for(int i = 0; i < NLin; i++){
+		for(int j = 0; j < NCol; j++){
+			if (Grid[i][j] != rhs.Grid[i][j]){
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 
 void Gen::Print(std::ostream &out, char alive, char lines)
 {
-
-  for (int i = 0; i < NLin; ++i) {
-    for (int j = 0; j < NCol; ++j) {
-
-    if ( Grid[i][j] == 1 ){
-        out << alive;
-      }
-      else {
-        out << lines;
-      }
-    }
-    out << std::endl;
-    }
-    out << std::endl;
-
+	for (int i = 0; i < NLin; ++i) {
+		for (int j = 0; j < NCol; ++j) {
+			if ( Grid[i][j] == 1 ){
+				out << alive;
+			}
+			else {
+				out << lines;
+			}
+		}
+		out << std::endl;
+	}
+	out << std::endl;
 }
 
 bool Gen::Extinct()
--- src/main.cpp
+++ src/main.cpp
@@ -12,80 +12,80 @@ Gen input(std::string filename);
 
 int main(int argc, char *argv[])
 {
-  
-  	if (argc != 2){
-		std::cout << "Please execute like this : ./game input.dat " << std::endl;
-    } 
-  std::vector<Gen> Try;
-  Gen gen1 = input(argv[1]);
-  Try.push_back(gen1);
-  std::ofstream ofs("historico.txt");
-  int linha, coluna;
+	if (argc != 2){
+	std::cout << "Please execute like this : ./game input.dat " << std::endl;
+	} 
+	std::vector<Gen> Try;
+	Gen gen1 = input(argv[1]);
+	Try.push_back(gen1);
+	std::ofstream ofs("historico.txt");
+	int linha, coluna;
 
-  int i = 0;
-  std::string entry_dump;
-  system("clear");
-  Try[i].Print();
-  i++;
-  Update(Try);
-  std::cout << "Wanna see the next generation? press y. Or n if you don't" << std::endl;
-  while(std::cin >> entry_dump){
+	int i = 0;
+	std::string entry_dump;
+	system("clear");
+	Try[i].Print();
+	i++;
+	Update(Try);
+	std::cout << "Wanna see the next generation? press y. Or n if you don't" << std::endl;
+	while(std::cin >> entry_dump){
 
-   	if(entry_dump[0] == 'n' or entry_dump[0] == 'N')
-	{
-		return 0;	
-	}
-	else{
-    system("clear");
-    Try[i].Print();
-    Try[i].Print(ofs);
-    if((Try[i].Extinct())){
-      std::cout << "Extinct at Generation " << i+1 << std::endl;
-      break;
-    }
-    int stability = Stable(Try);
+   		if(entry_dump[0] == 'n' or entry_dump[0] == 'N')
+		{
+			return 0;	
+		}
+		else{
+			system("clear");
+			Try[i].Print();
+			Try[i].Print(ofs);
+
+			if((Try[i].Extinct())){
+				std::cout << "Extinct at Generation " << i+1 << std::endl;
+				break;
+			}
+			int stability = Stable(Try);
    
-   	if(stability >= 0){
-      std::cout <<" Stable from Generation " << stability<< " To Generation "<<i << std::endl;
-      break;
-    }
+   			if(stability >= 0){
+				std::cout <<" Stable from Generation " << stability<< " To Generation "<<i << std::endl;
+				break;
+			}
 
-    i++;
-    Update(Try);
-  std::cout << "Wanna see the next generation? press y. Or n if you don't" << std::endl;
-    std::cout << std::endl;
+		i++;
+		Update(Try);
+		std::cout << "Wanna see the next generation? press y. Or n if you don't" << std::endl;
+		std::cout << std::endl;
+		}
 	}
-  }
   return 0;
 }
 
 /*! Atualiza a configuraçao para a proxima geraçao conforme as regras do jogo */
 void Update(std::vector <Gen> &Game) 
 {
-  int count;
-  int index = Game.size()-1;
-  Gen dump(Game[index].Linhas(),Game[index].Colunas());
-  Game.push_back(dump);
-  for (int i = 0; i < Game[index].Linhas(); ++i) {
-    for (int j = 0; j < Game[index].Colunas(); ++j) {
+	int count;
+	int index = Game.size()-1;
+	Gen dump(Game[index].Linhas(),Game[index].Colunas());
+	Game.push_back(dump);
+	for (int i = 0; i < Game[index].Linhas(); ++i) {
+		for (int j = 0; j < Game[index].Colunas(); ++j) {
 
-      count = Game[index].NeighborsCount(i,j);
+			count = Game[index].NeighborsCount(i,j);
 
-      if(Game[index](i,j)){
-        if(count < 2 or count > 3){
-          Game[index +1 ].Death(i,j);
-        }
-        else if(Game[index](i,j)){
-          Game[index +1].Birth(i,j); /* as celulas que não morreram permanecem vivas */
-        }
-      }
-      else if(count == 3) {
-        Game[index +1 ].Birth(i,j);
+			if(Game[index](i,j)){
+				if(count < 2 or count > 3){
+					Game[index +1 ].Death(i,j);
+				}
+				else if(Game[index](i,j)){
+					Game[index +1].Birth(i,j); /* as celulas que não morreram permanecem vivas */
+				}
+			}
+			else if(count == 3) {
+				Game[index +1 ].Birth(i,j);
 
-      }
-    }
+			}
+		}
 
-  }
+	}
 
 }
 
